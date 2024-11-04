1a787e2bdcbd6dbd805e6b0b5ecccd70874ce4f3
MFaad1 <faadsardar123@gmail.com>
2024-11-04 20:01:54 +0500

    fixes

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -18,17 +18,17 @@ const Navbar = ({ onClickSignIn, onClickSignUp }) => {
   useEffect(() => {
     const handle_loggedin_user = () => {
       const user = JSON.parse(localStorage.getItem("loggedIn_user"));
-      if(user){
-        setuser(user) 
+      if (user) {
+        setuser(user)
 
-      } else{
-        setuser(null) 
+      } else {
+        setuser(null)
 
       }
     };
-  
+
     window.addEventListener("logged_user", handle_loggedin_user);
-  
+
     return () => {
       window.removeEventListener("logged_user", handle_loggedin_user);
     };
@@ -36,52 +36,52 @@ const Navbar = ({ onClickSignIn, onClickSignUp }) => {
 
 
 
-useEffect(() => {
-     const user = JSON.parse(localStorage.getItem("loggedIn_user"));
-      if(user){
-        setuser(user) 
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("loggedIn_user"));
+    if (user) {
+      setuser(user)
 
-      }
-}, [])
+    }
+  }, [])
 
 
 
   return (
 
     <>
-    <Grid container spacing={0}>
-      <Grid item xs={1} sm={1} md={1} lg={1} xl={1} />
-      <Grid item xs={10} sm={10} md={10} lg={10} xl={10}>
-        <div className="navbar-container">
-          <button className="navbar-logo">
-            <img src={Logo} alt="" />
-          </button>
-          <div>
-            {/* <Link className="navbar-explore">Locations</Link> */}
-    {/* <p className="navbar-explore navbar-location">Locations</p> */}
+      <Grid container spacing={0}>
+        <Grid item xs={1} sm={1} md={1} lg={1} xl={1} />
+        <Grid item xs={10} sm={10} md={10} lg={10} xl={10}>
+          <div className="navbar-container">
+            <button className="navbar-logo">
+              <img src={Logo} alt="" />
+            </button>
+            <div>
+              {/* <Link className="navbar-explore">Locations</Link> */}
+              {/* <p className="navbar-explore navbar-location">Locations</p> */}
 
-           {/*  <button className="navbar-search">
+              {/*  <button className="navbar-search">
               <img src={Search} />
             </button> */}
 
-            {
-              // true
-              user
-               ? <AvatarDemo name ={user.full_name.charAt(0)}/> : 
-              <>
-            <button className="navbar-sign-in-btn" onClick={onClickSignIn}>
-              Sign in
-            </button>
-            <button className="navbar-sign-up-btn" onClick={onClickSignUp}>
-              Sign up
-            </button>
-              </>
-            }
+              {
+                // true
+                user
+                  ? <AvatarDemo name={user.full_name.charAt(0)} /> :
+                  <>
+                    <button className="navbar-sign-in-btn" onClick={onClickSignIn}>
+                      Sign in
+                    </button>
+                    <button className="navbar-sign-up-btn" onClick={onClickSignUp}>
+                      Sign up
+                    </button>
+                  </>
+              }
+            </div>
           </div>
-        </div>
+        </Grid>
+        <Grid item xs={1} sm={1} md={1} lg={1} xl={1} />
       </Grid>
-      <Grid item xs={1} sm={1} md={1} lg={1} xl={1} />
-    </Grid>
 
 
 
